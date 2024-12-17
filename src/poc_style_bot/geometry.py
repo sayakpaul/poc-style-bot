63e5f15c7969a601f62fd3c5f8e23c23729e8892
sayakpaul <spsayakpaul@gmail.com>
2024-12-17 09:39:17 +0530

    update

--- src/poc_style_bot/geometry.py
+++ src/poc_style_bot/geometry.py
@@ -4,6 +4,4 @@ import math
 def circle_area(radius):
     return math.pi * radius**2
 
-
-def circle_circumference(radius):
-    return 2 * math.pi * radius
+def circle_circumference(radius): return 2 * math.pi * radius
